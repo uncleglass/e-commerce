388244f50229c968857c9303853ab52692d2b5d5
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-10-20 14:16:50 +0200

    test: Add User Controller tests

---
+++ src/test/java/pl/uncleglass/ecommerce/controllers/UserControllerTest.java
@@ -0,0 +1,74 @@
+package pl.uncleglass.ecommerce.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.uncleglass.ecommerce.model.persistence.User;
+import pl.uncleglass.ecommerce.model.persistence.repositories.CartRepository;
+import pl.uncleglass.ecommerce.model.persistence.repositories.UserRepository;
+import pl.uncleglass.ecommerce.model.requests.CreateUserRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UserControllerTest {
+    private UserController userController;
+    private final UserRepository userRepository = mock(UserRepository.class);
+    private final CartRepository cartRepository = mock(CartRepository.class);
+    private final BCryptPasswordEncoder passwordEncoder = mock(BCryptPasswordEncoder.class);
+
+    @BeforeEach
+    public void setUp() {
+        userController = new UserController(userRepository, cartRepository, passwordEncoder);
+    }
+
+    @Test
+    public void create_user_happy_path() {
+        when(passwordEncoder.encode("password")).thenReturn("thisIsHashed");
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("Test");
+        userRequest.setPassword("password");
+        userRequest.setConfirmPassword("password");
+
+        ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = response.getBody();
+
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("Test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+    }
+
+    @Test
+    public void create_user_with_short_password() {
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("Test");
+        userRequest.setPassword("pass");
+        userRequest.setConfirmPassword("pass");
+
+        ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void create_user_with_wrong_confirm_password() {
+        CreateUserRequest userRequest = new CreateUserRequest();
+        userRequest.setUsername("Test");
+        userRequest.setPassword("password");
+        userRequest.setConfirmPassword("passwords");
+
+        ResponseEntity<User> response = userController.createUser(userRequest);
+
+        assertNotNull(response);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+}
